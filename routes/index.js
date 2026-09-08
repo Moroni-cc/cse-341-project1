157897cb828fc0b403a518eeac9aa868fe1a9a70
Moroni Capcha <raulmoronicapchacadillo@gmail.com>
2026-09-08 12:39:16 -0500

    feat: complete database connection and contacts GET routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req, res) => { res.send('Hello World'); });
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => { console.log(`Running on port ${port}`) })
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Base de datos conectada y servidor corriendo en el puerto ${port}`);
+        });
+    }
+});
